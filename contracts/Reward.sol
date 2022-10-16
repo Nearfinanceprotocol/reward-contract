ea27c46e85ec96a8ab386581e353b4cd3602ac5e
developeruche <nweleuchenna2004@gmail.com>
2022-10-16 23:30:51 +0100

    developeruche: added the onlySnapMaster and onlyAdmin modifer

--- contracts/Reward.sol
+++ contracts/Reward.sol
@@ -1,34 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+/// @author nearfinanceprotocol(@developeruche)
+/// @notice This is the contract handling the reward mechainsm for the nearfinance protocol this would be using a snapshot mechanism
+contract Reward {
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    /**
+     * ===================================================
+     * ----------------- STATE VARIABLES -----------------
+     * ===================================================
+     */
+
+    address private admin;
+    address private snapMaster;
+
+
+    /**
+     * ===================================================
+     * ----------------- ERROR ---------------------------
+     * ===================================================
+     */
+
+    error NotSnapMaster();
+    error NotAdmin();
+
+
+    /**
+     * ===================================================
+     * ----------------- EVENTS --------------------------
+     * ===================================================
+     */
+
+    event SnapShotTaken(address snapMaster, uint256 time);
+
+
+
+    function onlySnapMaster() public view {
+        if(msg.sender != snapMaster) {
+            revert NotSnapMaster();
+        }
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function onlyAdmin() public view {
+        if(msg.sender != admin) {
+            revert NotAdmin();
+        }
     }
+
+
+
+
 }
